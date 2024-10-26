73930f35812e58fff1c2eab985d3e15f1b479c5f
Rohit raj <rohit1201.be21@chitkara.edu.in>
2024-10-26 06:38:21 +0530

    Add first route

---
+++ backend/src/index.ts
@@ -0,0 +1,47 @@
+import { Hono } from 'hono'
+
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import {sign} from 'hono/jwt'
+
+const app = new Hono<{
+  Bindings : {
+    DATABASE_URL:string,
+    JWT_SECRET:string
+  }
+}>()
+
+app.post('/api/v1/signup', async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate());
+
+  const body=await c.req.json();
+
+  const user=await prisma.user.create({
+    data:{
+      email:body.email,
+      password:body.password
+
+    },
+  })
+
+  const token=await sign({id:user.id},c.env.JWT_SECRET)
+
+
+  return c.json({token})
+})
+app.post('/api/v1/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+app.get('/api/v1/blog/:id', (c) => {
+  return c.text('Hello Hono!')
+})
+
+export default app
